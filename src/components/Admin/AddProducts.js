39c833327751120e44f8f1605b8d3edb10c22a43
Farukhrana14 <farukhrana14@gmail.com>
2021-04-02 21:44:54 +0600

    Add product- OK

--- src/components/Admin/AddProducts.js
+++ src/components/Admin/AddProducts.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./AddProducts.css";
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+
 
 const AddProducts = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const [imageURL, setIMageURL] = useState(null);
 
+    const handleImageUpload = (event) => {
+    // console.log(event.target.files[0]);
+    const imageData = new FormData();
+    imageData.set('key', 'd136f1b08c3a19d44bcad67e010bd23c');
+    imageData.append('image', event.target.files[0]);
     
-    return (
-        <div>
-            <h1>Add these products</h1>
-        </div>
-    );
+    axios.post('https://api.imgbb.com/1/upload', 
+    imageData)
+    .then(function (response) {
+      setIMageURL(response.data.data.display_url);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
+const onSubmit = (data) =>{
+    const eventData = {
+        name: data.productName,
+        price: data.price,
+        photoURL: imageURL
+   };
+   const url = `http://localhost:5000/addProduct`;
+
+   fetch(url,{
+    method: 'POST', 
+    headers: {
+    'content-type': 'application/json'
+    },
+    body: JSON.stringify(eventData)
+   })
+   .then(res => console.log(res.status))
+
+
+}
+
+  return (
+    <div>
+      <h3>Add Products</h3>
+      <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="productName" placeholder="Name of Product" ref={register({ required: true })} />
+        {errors.name && <span className="error">Product Name is required</span>}
+        <br />
+        <br/>
+        <input name="price" placeholder="Price of Product" ref={register({ required: true })} />
+        {errors.name && <span className="error">Price is required</span>}
+        <br />
+        <br/>
+        <input name="inputFile" type="file" onChange={handleImageUpload} ref={register({ required: true })} />
+        {errors.name && <span className="error">Price is required</span>}
+        <br/>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default AddProducts;
